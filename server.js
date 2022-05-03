1af84cd033d2ceb6a08e5b6712f0dc26fd119641
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-02 21:46:18 -0500

    got it to show the first three option results

--- server.js
+++ server.js
@@ -20,21 +20,52 @@ async function init(){
 async function awaitMySqlWithInquirer(){
     await init()
 
-    const [employees] =  await db.execute("select * from employee")
+    const [departments] =  await db.execute("select * from department")
+    const [employees] =  await db.execute("select * from employee")    
+    const [roles] =  await db.execute("select * from roles")
 
-    console.table(employees);
+    // console.table(employees);
     
-       const {employee} = await prompt([{
-                type: 'list',
-                name: 'employee',
-                message: 'What employee do you want to talk to?',
-                choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
-              }])
+    await prompt([
+      {
+        type: "list",
+        name: "firstChoice",
+        message: "What would you like to do first?",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+      },
+    ])
+  .then(function (responses) {
+    if (responses.firstChoice === "View all departments") {
+        console.table(departments)
+        console.log("departments")
+        return awaitMySqlWithInquirer()
         
-              console.log(employee)
+    }
+    else if (responses.firstChoice === "View all roles") {
+      console.table(roles);
+      console.log("departments")
+      return awaitMySqlWithInquirer()
+    }
+    else if (responses.firstChoice === "View all employees") {
+      console.table(employees);
+      awaitMySqlWithInquirer()
+    }
+    else {
+        console.log("4");
+    }
+    })
+    
+      //  const {department} = await prompt([{
+      //           type: 'list',
+      //           name: 'department',
+      //           message: 'What Would you like to do?',
+      //           choices: departments.map(department=> ({name:department.id + " "+ department.name, value: department}))
+      //         }])
+        
+      //         console.log(department)
 
               
-                /// write next sql statements here! you would do some sort of sql query after this
+      //           /// write next sql statements here! you would do some sort of sql query after this
               
     }
     
