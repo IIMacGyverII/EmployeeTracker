04c84bd7b9eb957fdd7a985f338636e8cd3c1a3e
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-03 22:29:34 -0500

    code clean up, tested and working

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ async function init(){
         console.log(" ========================================="),
         console.log("\x1b[33m%s\x1b[0m",`| *Connected to the employee_db database* |`),
         console.log(" =========================================",),
-        // console.log("\x1b[33m%s\x1b[0m", "Log Message")
-        // console.log('\x1b[36m%s\x1b[0m', 'I am cyan')
-        
       );
 
 
@@ -25,11 +22,6 @@ async function init(){
 async function mainMenuWithInquirer(){
     await init()
 
-    const [departments] =  await db.execute("select * from department")
-    const [employees] =  await db.execute("select * from employee")    
-    const [roles] =  await db.execute("select * from roles")
-
-    // console.table(employees);
         console.log(" ==================================================")
         console.log("|                     Main Menu                    |")
         console.log(" ==================================================")
@@ -41,9 +33,10 @@ async function mainMenuWithInquirer(){
         choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add A Employee", "Update A Employee Role"]
       },
     ])
+
   .then(function (responses) {
     if (responses.firstChoice === "View All Departments") {
-        return viewDepartments()
+      return viewDepartments()
         
     }
     else if (responses.firstChoice === "View All Roles") {
@@ -53,21 +46,21 @@ async function mainMenuWithInquirer(){
       return viewEmployees()
     }
     else if (responses.firstChoice === "Add A Department") {
-      addADepartment() 
+      return addADepartment() 
     }
     else if (responses.firstChoice === "Add A Role") {
-      addARole()
+      return addARole()
     }
     else if (responses.firstChoice === "Add A Employee") {
-      addAEmployee()
+      return addAEmployee()
     }
     else if (responses.firstChoice === "Update A Employee Role") {
-      updateAEmployeeRole()
+      return updateAEmployeeRole()
     }
     else {
         console.log("you've been dropped to limbo... please press ctrl-c to escape");
     }
-    })
+})
     
 async function addADepartment() {
   await init()
@@ -76,17 +69,18 @@ async function addADepartment() {
 
     console.table(departments);
     
-       const response = await prompt([{
-                type: 'input',
-                name: 'departmentTitle',
-                message: 'What is the new department?',
-              },
-              ])
-              db.execute(`INSERT INTO department (name)
-                        VALUES ("${response.departmentTitle}");`)
-              console.log(" ================================================="),
-              console.log("\x1b[31m%s\x1b[0m",`| New department ${response.departmentTitle} has been added! |`),
-              console.log(" ================================================="),
+      const response = await prompt([
+        {
+          type: 'input',
+          name: 'departmentTitle',
+          message: 'What is the new department?',
+        },
+        ])
+        db.execute(`INSERT INTO department (name)
+                  VALUES ("${response.departmentTitle}");`)
+        console.log(" ================================================="),
+        console.log("\x1b[31m%s\x1b[0m",`| New department ${response.departmentTitle} has been added! |`),
+        console.log(" ================================================="),
   viewDepartments()
 }
 
@@ -98,30 +92,30 @@ async function addARole() {
 
     console.table(roles);
     
-       const response = await prompt([{
-                type: 'input',
-                name: 'roleTitle',
-                message: 'What is the new role?',
-              },
-              {
-                type: 'input',
-                name: 'salary',
-                message: 'What is the salary for the new role?',
-              },
-              {
-                type: 'list',
-                message: 'To what department does it belong to?',
-                name: 'chooseDepartment',
-                choices: departments.map(department=> ({name:department.name, value: department}))
-                },
-              ])
-              db.execute(`INSERT INTO roles (title, salary, department_id)
-                        VALUES ("${response.roleTitle}", "${response.salary}", ${response.chooseDepartment.id});`)
-              console.log(" =============================================================================="),
-              console.log("\x1b[31m%s\x1b[0m",`| New role ${response.roleTitle} with a salary of $${response.salary} belonging to department ${response.chooseDepartment.id} has been added! |`),
-              console.log(" =============================================================================="),
+      const response = await prompt([
+        {
+          type: 'input',
+          name: 'roleTitle',
+          message: 'What is the new role?',
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'What is the salary for the new role?',
+        },
+        {
+          type: 'list',
+          message: 'To what department does it belong to?',
+          name: 'chooseDepartment',
+          choices: departments.map(department=> ({name:department.name, value: department}))
+          },
+        ])
+        db.execute(`INSERT INTO roles (title, salary, department_id)
+                  VALUES ("${response.roleTitle}", "${response.salary}", ${response.chooseDepartment.id});`)
+        console.log(" ==============================================================================================="),
+        console.log("\x1b[31m%s\x1b[0m",`| New role (${response.roleTitle}) with a salary of $${response.salary} belonging to the ${response.chooseDepartment.name}(${response.chooseDepartment.id}) department has been added! |`),
+        console.log(" ==============================================================================================="),
   viewRoles()
-              
 }
 
 async function addAEmployee() {
@@ -132,37 +126,35 @@ async function addAEmployee() {
 
     console.table(employees);
     
-       const response = await prompt([{
-                type: 'input',
-                name: 'firstName',
-                message: 'What is the employees first name?',
-                // choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
-              },
-              {
-                type: 'input',
-                name: 'lastName',
-                message: 'What is the employees last name?',
-                // choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
-              },
-              {
-                type: 'list',
-                message: 'Choose the employees Role',
-                name: 'chooseRole',
-                choices: roles.map(role=> ({name:role.id + ": "+ role.title, value: role}))
-                },
-                {
-                  type: 'list',
-                  message: 'Choose the employees manager',
-                  name: 'chooseManager',
-                  choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
-                  },
-              ])
-              db.execute(`INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-                        VALUES ("${response.firstName}", "${response.lastName}", ${response.chooseRole.id}, ${response.chooseManager.id});`)
-              // console.table(employees)
-              console.log(" ================================================="),
-              console.log("\x1b[31m%s\x1b[0m",`| Employee ${response.firstName} ${response.lastName} with a role of ${response.chooseRole.id} has been added! |`),
-              console.log(" ================================================="),
+      const response = await prompt([
+        {
+          type: 'input',
+          name: 'firstName',
+          message: 'What is the employees first name?',
+        },
+        {
+          type: 'input',
+          name: 'lastName',
+          message: 'What is the employees last name?',
+        },
+        {
+          type: 'list',
+          message: 'Choose the employees Role',
+          name: 'chooseRole',
+          choices: roles.map(role=> ({name:role.id + ": "+ role.title, value: role}))
+        },
+        {
+          type: 'list',
+          message: 'Choose the employees manager',
+          name: 'chooseManager',
+          choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
+        },
+        ])
+        db.execute(`INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+                    VALUES ("${response.firstName}", "${response.lastName}", ${response.chooseRole.id}, ${response.chooseManager.id});`)
+        console.log(" ================================================="),
+        console.log("\x1b[31m%s\x1b[0m",`| Employee ${response.firstName} ${response.lastName} with a role of ${response.chooseRole.id} has been added! |`),
+        console.log(" ================================================="),
   viewEmployees();
 }
 
@@ -175,26 +167,27 @@ async function updateAEmployeeRole() {
 
     console.table(employees);
     
-        const response = await prompt([{
-          type: 'list',
-          name: 'employee',
-          message: 'Which employees role would you like to update?',
-          choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
-        },
-        {
-          type: 'list',
-          message: 'What would you like their new role to be?',
-          name: 'chooseRole',
-          choices: roles.map(role=> ({name:role.id + ": "+ role.title, value: role}))
+        const response = await prompt([
+          {
+            type: 'list',
+            name: 'employee',
+            message: 'Which employees role would you like to update?',
+            choices: employees.map(employee=> ({name:employee.first_name + " "+ employee.last_name, value: employee}))
           },
-        ])
-        db.execute( `UPDATE employee SET roles_id =${response.chooseRole.id} WHERE id = ${response.employee.id}`)
-        console.table(employees)
-        console.log(" ================================================="),
-        console.log("\x1b[31m%s\x1b[0m",`| Employee ${response.employee.first_name} ${response.employee.last_name}s role has been changed to ${response.chooseRole.id} |`),
-        console.log(" ================================================="),
-        await mainMenuWithInquirer();
-    }
+          {
+            type: 'list',
+            message: 'What would you like their new role to be?',
+            name: 'chooseRole',
+            choices: roles.map(role=> ({name:role.id + ": "+ role.title, value: role}))
+            },
+          ])
+          db.execute( `UPDATE employee SET roles_id =${response.chooseRole.id} WHERE id = ${response.employee.id}`)
+          console.table(employees)
+          console.log(" ================================================="),
+          console.log("\x1b[31m%s\x1b[0m",`| Employee ${response.employee.first_name} ${response.employee.last_name}s role has been changed to ${response.chooseRole.id} |`),
+          console.log(" ================================================="),
+  viewEmployees()
+}
 
 async function viewRoles() {
   const [roles] =  await db.execute("select * from roles")
@@ -222,29 +215,11 @@ async function viewEmployees() {
   console.table(employees);
   return mainMenuWithInquirer()
 }
-
-
-
-
-
-
-
-  // return mainMenuWithInquirer()
-
-
-
-// async function awaitWithInquirerByItself(){
- 
-    
-//        const {size} = await prompt([{
-//                 type: 'list',
-//                 name: 'size',
-//                 message: 'What size do you need?',
-//                 choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro']
-//               }])
-        
-//           console.log(size);
-        
-//     }
-
 }
+
+
+
+
+
+
+
